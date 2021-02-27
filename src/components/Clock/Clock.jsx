03f253d98bfc8f450161622cbf7f3e73ada99fff
tpreston96 <tpreston96@gmail.com>
2021-02-27 17:28:53 -0600

    formatting

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -2,16 +2,20 @@ import React, { useState, useEffect } from "react";
 
 const Clock = () => {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
-  const [date, setDate] = useState(new Date().toDateString())
+  const [date, setDate] = useState(new Date().toDateString());
   useEffect(() => {
     setInterval(() => {
       setTime(new Date().toLocaleTimeString());
-      setDate(new Date().toDateString())
+      setDate(new Date().toDateString());
     }, 1000);
   });
   return (
     <div className="time" style={{ textAlign: "center" }}>
-      <h3>Today's Date: <br/>{date} <br/>{time}</h3>
+      <h3>
+        Today's Date: <br />
+        {date} <br />
+        {time}
+      </h3>
     </div>
   );
 };
