f6ae6758dacf5e4b9710946c4c6775ae9d92a508
tpreston96 <tpreston96@gmail.com>
2021-02-26 18:55:12 -0600

    reformat to bootstrap pt1

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,6 +6,65 @@ import "./NavBar.css";
 const NavBar = ({ user, handleLogout }) => {
   let nav = user ? (
     <>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">
+          <b>AniApp</b>
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Profile
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Users
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Anime
+              </a>
+              <div
+                class="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <a class="dropdown-item" href="/anime">
+                  Anime List
+                </a>
+                <a class="dropdown-item" href="#">
+                  Add Anime
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
       <nav className="nav-bar">
         <div className="nav-wrapper">
           <ul className="right">
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,20 +1,22 @@
-import React, { useState } from "react";
-import { Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
 
 import NavBar from "../../components/NavBar/NavBar";
 
-import LoginPage from '../LoginPage/LoginPage';
-import SignupPage from '../SignupPage/SignupPage';
-import ForgotPasswordPage from '../ForgotPasswordPage/ForgotPasswordPage'
-import ResetPasswordPage from '../ResetPasswordPage/ResetPasswordPage'
+import LoginPage from "../LoginPage/LoginPage";
+import SignupPage from "../SignupPage/SignupPage";
+import ForgotPasswordPage from "../ForgotPasswordPage/ForgotPasswordPage";
+import ResetPasswordPage from "../ResetPasswordPage/ResetPasswordPage";
 
-import userService from '../../services/userService';
-
+import userService from "../../services/userService";
+import tokenService from "../../services/tokenService";
 import "./App.css";
 
 const App = () => {
-  const [user, setUser] = useState('');
-
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    setUser(tokenService.getUserFromToken());
+  }, []);
   const handleLogout = () => {
     userService.logout();
     setUser(null);
@@ -25,53 +27,61 @@ const App = () => {
   };
 
   const determineError = (msg) => {
-    if (msg.includes('Error')) return 'red-text'
-    return 'green-text'
-  }
+    if (msg.includes("Error")) return "red-text";
+    return "green-text";
+  };
 
   return (
     <>
-      <NavBar 
-        user={user}
-        handleLogout={handleLogout}
-      />
+      <NavBar user={user} handleLogout={handleLogout} />
 
       <Switch>
-        <Route exact path="/login" render={({history}) => 
-          <>
-            <LoginPage 
-              history={history}
-              handleSignupOrLogin={handleSignupOrLogin}
-              determineError={determineError}
-            />
-          </>
-        }></Route>
+        <Route
+          exact
+          path="/login"
+          render={({ history }) => (
+            <>
+              <LoginPage
+                history={history}
+                handleSignupOrLogin={handleSignupOrLogin}
+                determineError={determineError}
+              />
+            </>
+          )}
+        ></Route>
 
-        <Route exact path="/signup" render={({history}) => 
-          <>
-            <SignupPage 
-              history={history}
-              handleSignupOrLogin={handleSignupOrLogin}
-              determineError={determineError}
-            />
-          </>
-        }></Route>
+        <Route
+          exact
+          path="/signup"
+          render={({ history }) => (
+            <>
+              <SignupPage
+                history={history}
+                handleSignupOrLogin={handleSignupOrLogin}
+                determineError={determineError}
+              />
+            </>
+          )}
+        ></Route>
 
-        <Route exact path='/forgot' render={() => 
-          <>
-            <ForgotPasswordPage 
-              determineError={determineError}
-            />
-          </>
-        }></Route>
+        <Route
+          exact
+          path="/forgot"
+          render={() => (
+            <>
+              <ForgotPasswordPage determineError={determineError} />
+            </>
+          )}
+        ></Route>
 
-        <Route path='/resetpassword/:token' render={() => 
-          <>
-            <ResetPasswordPage 
-              determineError={determineError}
-            />
-          </>
-        }></Route>
+        <Route
+          path="/resetpassword/:token"
+          render={() => (
+            <>
+              <ResetPasswordPage determineError={determineError} />
+            </>
+          )}
+        ></Route>
       </Switch>
     </>
   );
