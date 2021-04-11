962831b827ba3bd7275617ffadc83f347afc0621
tpreston96 <tpreston96@gmail.com>
2021-04-10 19:40:57 -0500

    user page

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
+import "./Users.css";
 import userService from "../../services/userService";
 
 const Users = ({ user }) => {
   const [users, setUsers] = useState([]);
 
-  
   useEffect(() => {
     // IIFE - Immediately Invoked Function Expression
     (async () => {
@@ -13,7 +13,27 @@ const Users = ({ user }) => {
     })();
   }, [setUsers]);
 
-  return <div>Users Page</div>;
+  return (
+    <div>
+      <h1>Users</h1>
+      <div className="userDiv">
+        {users.map((user) => {
+          return (
+            <div className="card user">
+              <div className="card-header">
+                <p>
+                  <b>{user.name}</b>
+                </p>
+              </div>
+              <p>Email: {user.email}</p>
+              <p>Joined: {user.createdAt}</p>
+              <button className="btn gold">friend button</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Users;
