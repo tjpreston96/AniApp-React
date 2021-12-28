7ffdafaa7da931507dbaaab7e13dc7ed620e5fcf
Tyler Preston <tpreston96@gmail.com>
2021-12-28 14:01:48 -0600

    formatting

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,28 +1,28 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
-import LoginForm from "../../components/LoginForm/LoginForm"
+import LoginForm from "../../components/LoginForm/LoginForm";
 
-import "./LoginPage.css"
+import "./LoginPage.css";
 
 const LoginPage = ({ history, handleSignupOrLogin, determineError }) => {
-    const [message, setMessage] = useState("")
-    const [color, setColor] = useState("")
+  const [message, setMessage] = useState("");
+  const [color, setColor] = useState("");
 
-    const updateMessage = (msg) => {
-        setMessage(msg)
-        setColor(determineError(msg))
-    }
+  const updateMessage = (msg) => {
+    setMessage(msg);
+    setColor(determineError(msg));
+  };
 
-    return (
-        <div className="LoginPage">
-            <LoginForm
-                history={history}
-                handleSignupOrLogin={handleSignupOrLogin}
-                updateMessage={updateMessage}
-            />
-            <p className={color}>{message}</p>
-        </div>
-    )
-}
+  return (
+    <div className="LoginPage">
+      <LoginForm
+        history={history}
+        handleSignupOrLogin={handleSignupOrLogin}
+        updateMessage={updateMessage}
+      />
+      <p className={color}>{message}</p>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
