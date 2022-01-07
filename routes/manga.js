9cf43fc55f516aef1b9125e15c1951e7f5083c73
Tyler Preston <tpreston96@gmail.com>
2022-01-07 12:27:01 -0600

    manga routes

--- routes/manga.js
+++ routes/manga.js
@@ -3,11 +3,11 @@ const mangaCtrl = require("../controllers/manga");
 
 router.use(require("../config/auth"));
 
-router.get("/:id", isLoggedIn, mangaCtrl.index);
+// router.get("/:id", isLoggedIn, mangaCtrl.index);
 router.post("/search", isLoggedIn, mangaCtrl.search);
-router.get("/:slug", isLoggedIn, mangaCtrl.show);
-router.post("/:slug/collection", isLoggedIn, mangaCtrl.addToCollection);
-router.delete("/:slug/collection", isLoggedIn, mangaCtrl.removeFromCollection);
+// router.get("/:slug", isLoggedIn, mangaCtrl.show);
+// router.post("/:id/collection", isLoggedIn, mangaCtrl.addToCollection);
+// router.delete("/:id/collection", isLoggedIn, mangaCtrl.removeFromCollection);
 
 function isLoggedIn(req, res, next) {
   console.log(req.user)
