0f8ad909a46fdbd9d32e01adb99c96823ec38978
Tyler Preston <tpreston96@gmail.com>
2022-01-07 12:30:02 -0600

    services reformat

--- src/services/mediaService.js
+++ src/services/mediaService.js
@@ -1,26 +1,63 @@
 import tokenService from "../services/tokenService";
 
-export function addToCollection(formData, type) {
-  return fetch(`/api/${type}/${formData.slug}/collection`, {
+  export function addToCollection(formData) {
+    return fetch(`/api/media/${formData.id}/add`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "Authorization": "Bearer " + tokenService.getToken(),
+      },
+      body: JSON.stringify(formData),
+    }).then((res) => res.json());
+  }
+  
+  export function removeFromCollection(formData) {
+      return fetch(`/api/media/${formData.id}/remove`, {
+        method: "DELETE",
+        headers: {
+          "content-type": "application/json",
+          "Authorization": "Bearer " + tokenService.getToken(),
+        },
+      }).then((res) => res.json());
+    }
+  
+
+
+
+export function index() {
+  return fetch(`/api/media/`, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      "Authorization": "Bearer " + tokenService.getToken(),
+    },
+  }).then((res) => res.json());
+}
+
+export function mangaIndex() {
+  return fetch(`/api/media/manga`, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      "Authorization": "Bearer " + tokenService.getToken(),
+    },
+  }).then((res) => res.json());
+}
+
+export function mangaCollection(collection) {
+  return fetch(`/api/media/manga/collection`, {
     method: "POST",
     headers: {
       "content-type": "application/json",
       "Authorization": "Bearer " + tokenService.getToken(),
     },
-    body: JSON.stringify(formData),
+    body: JSON.stringify({collection})
   }).then((res) => res.json());
 }
 
-export function removeFromCollection(formData, type) {
-    return fetch(`/api/${type}/${formData.slug}/collection`, {
-      method: "DELETE",
-      headers: {
-        "content-type": "application/json",
-        "Authorization": "Bearer " + tokenService.getToken(),
-      },
-    }).then((res) => res.json());
-  }
 
+
+// !Probably not needed
 export function showDetail(slug, type) {
   return fetch(`/api/${type}/${slug}`, {
     headers: {
@@ -28,7 +65,6 @@ export function showDetail(slug, type) {
     },
   }).then((res) => res.json());
 }
-
 
 export function animeIdx() {
   return fetch(`/api/anime/`, {
