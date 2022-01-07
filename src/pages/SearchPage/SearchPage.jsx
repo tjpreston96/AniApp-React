49d4edbee2b9c5a5fc322fa2c0674dea5e6881f9
Tyler Preston <tpreston96@gmail.com>
2022-01-07 12:29:35 -0600

    add params to search page

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import ResultsList from "../../components/ResultsList/ResultsList";
+import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
   const [results, setResults] = useState([]);
+  const { category } = useParams();
+
+  useEffect(() => {
+    setResults([]);
+  }, [category]);
 
   return (
     <>
       <SearchBar setResults={setResults} />
-      <ResultsList results={results} setResults={setResults} />
+      <ResultsList results={results} />
     </>
   );
 };
