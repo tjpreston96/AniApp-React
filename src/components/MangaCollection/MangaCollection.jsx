55142b2d879c535eea197e2117795248d87c23ac
Tyler Preston <tpreston96@gmail.com>
2022-01-07 12:27:53 -0600

    manga collection rendered

--- src/components/MangaCollection/MangaCollection.jsx
+++ src/components/MangaCollection/MangaCollection.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { mangaIndex, mangaCollection } from "../../services/mediaService";
+import ResultsList from "../../components/ResultsList/ResultsList";
 
 const MangaCollection = () => {
+  // console.log(index())
+  const [collection, setCollection] = useState();
+
+  useEffect(() => {
+    async function setupCollection() {
+      const index = await mangaIndex();
+      const c = await mangaCollection(index.join());
+      setCollection(c);
+    }
+    setupCollection();
+  }, []);
+
+  
+
   return (
     <>
       <h1>Manga Collection</h1>
+      <ResultsList results={collection} />
     </>
   );
 };
